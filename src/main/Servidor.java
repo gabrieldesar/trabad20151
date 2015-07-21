d5da2aec6ff67683e1461a007d99baed959a32b5
Felipe Milepe <fmilepe@gmail.com>
2015-07-21 10:35:08 -0300

    Adicionando método estático ao servidor para calcular o temp ode servico para um dado cliente.

--- src/main/Servidor.java
+++ src/main/Servidor.java
@@ -1,7 +1,17 @@
 package main;
 
-public class Servidor {
-	
-	
+public class Servidor
+{
 
+	private static final double FATOR_TRUNCAMENTO_TEMPO = 10;
+
+	public static long geraTempoDeServico(float mi)
+	{
+		if (mi > 0)
+		{
+			double tempoServico = -Math.log(1 - (1 - Math.exp(-mi)) * Math.random()) / mi;
+			return (int) Math.round(tempoServico * FATOR_TRUNCAMENTO_TEMPO);
+		}
+		return 0;
+	}
 }
