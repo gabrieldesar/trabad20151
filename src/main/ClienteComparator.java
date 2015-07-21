dccaa05743383ab4ebd4004d26e603e11b0d3383
Felipe Milepe <fmilepe@gmail.com>
2015-07-21 10:36:13 -0300

    Começando código da simulação propriamente dita e criando Classe comparator para ordenar a lista de clientes, se necessário.

---
+++ src/main/ClienteComparator.java
@@ -0,0 +1,19 @@
+package main;
+
+import java.util.Comparator;
+
+public class ClienteComparator implements Comparator<Cliente>
+{
+
+	@Override
+	public int compare(Cliente c1, Cliente c2)
+	{
+		if(c1.tempoChegada < c2.tempoChegada)
+			return -1;
+		if(c1.tempoChegada == c2.tempoChegada)
+			return 0;
+		else
+			return 1;
+	}
+
+}
--- src/main/Simulador.java
+++ src/main/Simulador.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public abstract class Simulador {
@@ -10,14 +11,18 @@ public abstract class Simulador {
 	float mi;
 	float p;
 	
-	Servidor servidor;
-	List<Cliente> clientes = new ArrayList<Cliente>();
+	long tempoSimulacao;
 	
-	public Simulador(float lambda, float mi, float p){
+	
+	Servidor servidor;
+	Cliente proximaChegada = null;
+	List<Cliente> clientesNoSitema = new ArrayList<Cliente>();
+	
+	public Simulador(float lambda, float mi, float p, long tempoSimulacao){
 		this.lambda = lambda;
 		this.mi = mi;
 		this.p = p;		
-		
+		this.tempoSimulacao = tempoSimulacao;
 	}
 	
 	//Gerar um cliente novo que entra no sistema
@@ -25,6 +30,21 @@ public abstract class Simulador {
 	
 	//Loop do processamento da rede de filas
 	public void simula(){
+		long clientesNoSistema = 0;
+		//Cliente
+		
+		for(long i = 1; i <= tempoSimulacao; i++) {
+			if(proximaChegada == null) {
+				proximaChegada = new Cliente(this.entradaCliente(),Servidor.geraTempoDeServico(mi));
+			} else {
+				if(proximaChegada.tempoChegada == i) {
+					clientesNoSitema.add(proximaChegada);
+					Collections.sort(clientesNoSitema, new ClienteComparator());
+					proximaChegada = null;
+				}
+			}
+		}
+		
 		
 	}
 	
