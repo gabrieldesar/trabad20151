656f8e3fa9735b81e92e7128b77c971d0b01ee5a
Felipe Milepe <fmilepe@gmail.com>
2015-07-21 10:32:52 -0300

    Criando atributos da classe cliente.

--- src/main/Cliente.java
+++ src/main/Cliente.java
@@ -2,4 +2,12 @@ package main;
 
 public class Cliente {
 
+	long tempoChegada;
+	long tempoServico;
+	long tempoSaida;
+	
+	public Cliente (long tempoChegada, long tempoServico) {
+		this.tempoChegada = tempoChegada;
+		this.tempoServico = tempoServico;
+	}
 }
